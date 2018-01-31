f3fdfc44228bc3a808c35103b6a06f673db5d76e
Kevin Li <kevinjcli@hotmail.com>
2018-01-31 21:14:17 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,28 @@
 function add (a, b) {
-  var sum
-  sum = a + b 
-  return sum
+  var total
+  total = a + b 
+  return total
 }
+
+function subtract (a, b) {
+  var total
+  total = a - b
+  return total
+}
+
+function multiply (a, b) {
+  var total
+  total = a * b 
+  return total 
+}
+
+function divide (a, b) {
+  var total 
+  total = a / b 
+  return total
+}
+
+
+
+
+
